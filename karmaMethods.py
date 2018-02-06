59da01a89c1100a1d86ba68975d611c8a3d75aa7
mani1019 <32697148+mani1019@users.noreply.github.com>
2018-02-06 22:53:57 +0100

    Update karmaMethods.py

--- karmaMethods.py
+++ karmaMethods.py
@@ -1,57 +1,78 @@
-import operator,pickle
-users = {"Franky":0, "@TheGlitchHam":0,"@dawu1986":0,"@Jominator":0,"@Atera97":0,"@Schlaumayer":0,"@TerenceChill":0,"@DonDyaego":0,"Buffy":0}
+from telegram.ext import Updater,CommandHandler
+import logging,karmamethods as k
+import newsmethods as n
+updater = Updater(token='361500602:AAEPtaMTw0xhcJFqVyFz6D33kmMI3Liba8s')
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-def register(bot, update,args):
-    if args[0] in users:
-      bot.sendMessage(chat_id=update.message.chat_id, text="User is already registered")
-    else:
-        users[args[0]]=0
-        bot.sendMessage(chat_id=update.message.chat_id, text=args[0]+" is now registered")
-
-def getUsers(bot, update):
-    message="Registered users are:\n"
-    for name in users:
-        message=message+name+" "
-    bot.sendMessage(chat_id=update.message.chat_id, text=message)
-
-def top(bot, update):
-    message="Top 10 users are:\n"
-    sorted_x = sorted(users.items(), key=operator.itemgetter(1), reverse=True)
-    for i in range(10):
-        if i<len(sorted_x):
-            message=message+str(sorted_x[i])+"\n"
-    bot.sendMessage(chat_id=update.message.chat_id, text=message)
+def help(bot, update):
+    handlerGroups = dispatcher.handlers
+    handlers=handlerGroups.get(0)
+    string='Commands:'
+    for handler in handlers:
+        string=string+'\n/'+str(handler.command)
+        if handler.pass_args:
+            string=string+' args'
+    string=string+'\n'+'code at https://github.com/Schlaumayer/heidelbot'
+    string=string+'\n'+'discord invite https://discord.gg/fgzsFwf'
+    bot.sendMessage(chat_id=update.message.chat_id, text=string)   
     
-def plus(bot, update, args):
-    if args[0] in users:
-       karma=users[args[0]]
-       karma=karma+1
-       users[args[0]]=karma
-       try:
-        bot.sendMessage(chat_id=update.message.chat_id, text="Karma of '"+args[0]+"' is now "+str(karma))
-       except TelegramError:
-        bot.sendMessage(chat_id=update.message.chat_id, text="KKarma of '"+args[0]+"' is now "+str(karma))
-    else:
-       bot.sendMessage(chat_id=update.message.chat_id, text=args[0]+" is no registered user. Please use /register")
-       
+def dump(bot, update):
+    n.dumpNews(bot, update)
+    k.dumpKarma(bot, update)
 
-def minus(bot, update, args):
-    if args[0] in users:
-       karma=users[args[0]]
-       if karma > 0:
-           karma=karma-1
-       users[args[0]]=karma
-       bot.sendMessage(chat_id=update.message.chat_id, text="Karma of '"+args[0]+"' is now "+str(karma))
-    else:
-        bot.sendMessage(chat_id=update.message.chat_id, text=args[0]+" is no registered user. Please use /register")
-       
-def dumpKarma(bot, update):
-    with open('karmafile.pickle', 'wb') as fp:
-        pickle.dump(users, fp)
-        bot.sendMessage(chat_id=update.message.chat_id, text='Karma in Datei abgelegt.')
+def load(bot, update):
+    n.loadNews(bot, update)
+    k.loadKarma(bot, update)
 
-def loadKarma(bot, update):
-    with open ('karmafile.pickle', 'rb') as fp:
-        karmaList = pickle.load(fp)
-    users.update(karmaList)
-    bot.sendMessage(chat_id=update.message.chat_id, text='Karma aus Datei gelesen.')
+def bamboozle(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="Bamboozled!!!")
+    
+plus_handler = CommandHandler('+', k.plus,pass_args=True)
+plus_handler2 = CommandHandler('plus', k.plus,pass_args=True)
+dispatcher.add_handler(plus_handler)                       
+dispatcher.add_handler(plus_handler2)
+
+minus_handler = CommandHandler('-', k.minus,pass_args=True)
+minus_handler2 = CommandHandler('minus', k.minus,pass_args=True)
+dispatcher.add_handler(minus_handler)
+dispatcher.add_handler(minus_handler2)
+
+register_handler = CommandHandler('register', k.register,pass_args=True)
+dispatcher.add_handler(register_handler)
+
+unregister_handler = CommandHandler('unregister', k.unregister,pass_args=True)
+dispatcher.add_handler(unregister_handler)
+
+getUsers_handler = CommandHandler('getusers', k.getUsers)
+dispatcher.add_handler(getUsers_handler)
+
+top_handler = CommandHandler('top', k.top)
+dispatcher.add_handler(top_handler)
+
+getnews_handler = CommandHandler('getnews', n.getNews)
+dispatcher.add_handler(getnews_handler)
+
+addnews_handler = CommandHandler('addnews', n.addNews,pass_args=True)
+dispatcher.add_handler(addnews_handler)
+
+removenews_handler = CommandHandler('removenews', n.removeNews,pass_args=True)
+dispatcher.add_handler(removenews_handler)
+
+removeallnews_handler = CommandHandler('removeallnews', n.removeAllNews)
+dispatcher.add_handler(removeallnews_handler)
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
+
+bam_handler = CommandHandler('anspruchsvolle', bamboozle)
+dispatcher.add_handler(bam_handler)
+
+dump_handler = CommandHandler('dump', dump)
+dispatcher.add_handler(dump_handler)
+
+load_handler = CommandHandler('load', load)
+dispatcher.add_handler(load_handler)
+
+               
+updater.start_polling()
